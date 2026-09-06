d55a763997e519419188ffbdbd9bc8983f56a011
Bello Olamilekan <belloolamilekan661@gmail.com>
2026-09-06 14:55:11 +0100

    first commit

---
+++ Introduction/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Introduction;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to c# documentation guide walk through!!! Olamilekan.");
+    }
+}
