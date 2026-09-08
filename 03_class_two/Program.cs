9d59e4371cf23671002aef601a40d41199980cf5
Bello Olamilekan <belloolamilekan661@gmail.com>
2026-09-08 09:09:27 +0100

    simple method lesson

--- 03_class_two/Program.cs
+++ 03_class_two/Program.cs
@@ -8,21 +8,23 @@ class Program
         // int myInteger = 10;
         // myInteger = myInteger * myInteger;
         // Console.WriteLine(myInteger);
-
-        int userLeftValue;
-        int userRightValue;
-        string usersTextInput;
-        int result;
-        
-        Console.Write("Enter value 1: ");
-        usersTextInput = Console.ReadLine();
-        userLeftValue = int.Parse(usersTextInput);
-        
-        Console.Write("Enter value 2: ");
-        usersTextInput = Console.ReadLine();
-        userRightValue = int.Parse(usersTextInput);
-        
-        result = userLeftValue + userRightValue;
-        Console.WriteLine($"{userLeftValue} + {userRightValue} = {result}");
+        //
+        // int userLeftValue;
+        // int userRightValue;
+        // string usersTextInput;
+        // int result;
+        //
+        // Console.Write("Enter value 1: ");
+        // usersTextInput = Console.ReadLine();
+        // userLeftValue = int.Parse(usersTextInput);
+        //
+        // Console.Write("Enter value 2: ");
+        // usersTextInput = Console.ReadLine();
+        // userRightValue = int.Parse(usersTextInput);
+        //
+        // result = userLeftValue + userRightValue;
+        // Console.WriteLine($"{userLeftValue} + {userRightValue} = {result}");
+        //
+        // float myFloat = 1.34f;
     }
 }
---
+++ 06_methods/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _06_methods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Methods!");
+
+        decimal decimal1 = GetDecimalFromUser(prompt: "Enter Decimal 1: ");
+        decimal decimal2 = GetDecimalFromUser(prompt: "Enter Decimal 2: ");
+        decimal decimal3 = GetDecimalFromUser(prompt: "Enter Decimal 3: ");
+        decimal result = 0;
+        result = decimal1 + decimal2 + decimal3;
+        Console.WriteLine($"Result : {result}");
+            
+
+
+        static decimal GetDecimalFromUser(string prompt)
+        {
+            bool hasEnteredDecimal = false;
+            decimal decimalresult = 0;
+
+            while (!hasEnteredDecimal)
+            {
+                Console.Write(prompt);
+                hasEnteredDecimal = decimal.TryParse(Console.ReadLine(), out decimalresult);
+            }
+
+            return decimalresult;
+        }
+    }
+}
